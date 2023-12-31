27e2ff7c391251f51503f502b6a0a06300ae57c7
Sunil Kumar Singh <sunil_zee85@yahoo.com>
2024-01-01 01:05:47 +0530

    handle static files

--- solutions/settings.py
+++ solutions/settings.py
@@ -104,10 +104,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-MEDIA_URL = '/static/'
-MEDIA_ROOT = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
